bd2b4d4674b8e22330e35ac0caf1572d1faee4fa
swallowsssssakuraaaa-crypto <swallowssss.sakuraaaa@gmail.com>
2026-04-08 21:51:16 +0900

    check_tokudane.py を更新

--- check_tokudane.py
+++ check_tokudane.py
@@ -1,11 +1,13 @@
 import os
 import requests
 import datetime
+import json
 
 LINE_CHANNEL_TOKEN = os.environ["LINE_CHANNEL_TOKEN"]
 LINE_USER_ID = os.environ["LINE_USER_ID"]
 
 def send_line(message):
+
     url = "https://api.line.me/v2/bot/message/push"
 
     headers = {
@@ -26,34 +28,37 @@ def send_line(message):
     requests.post(url, headers=headers, json=data)
 
 today = datetime.date.today()
-target = today + datetime.timedelta(days=30)
 
-weekday = target.weekday()
+messages = []
 
-route = None
+for i in range(1,31):
 
-if weekday in [3,4]:
-    route = "東京 → 富山"
+    target = today + datetime.timedelta(days=i)
 
-if weekday in [6,0]:
-    route = "富山 → 東京"
+    weekday = target.weekday()
 
-if route:
+    # あなたの移動パターン
+    if weekday in [3,4,6,0]:
 
-    message = f"""
-🚄トクだ値監視中
+        messages.append(
+f"""
+🚄富山チャンス日
 
-対象日
+日付
 {target}
 
 区間
-{route}
+東京 ↔ 富山
 
 優先列車
-かがやき / はくたか
+かがやき
+はくたか
 
-👇予約
+👇検索
 https://www.eki-net.com/
 """
+        )
 
-    send_line(message)
+if messages:
+
+    send_line("\n".join(messages))

09481d42c5f6564feaae61d1457dcc6789a0e2b2
swallowsssssakuraaaa-crypto <swallowssss.sakuraaaa@gmail.com>
2026-04-08 21:59:31 +0900

    check_tokudane.py を更新

--- check_tokudane.py
+++ check_tokudane.py
@@ -5,6 +5,9 @@ import datetime
 LINE_CHANNEL_TOKEN = os.environ["LINE_CHANNEL_TOKEN"]
 LINE_USER_ID = os.environ["LINE_USER_ID"]
 
+LAST_FILE = "last_sent.txt"
+
+
 def send_line(message):
 
     url = "https://api.line.me/v2/bot/message/push"
@@ -27,45 +30,65 @@ def send_line(message):
     requests.post(url, headers=headers, json=data)
 
 
+def load_last():
+
+    if not os.path.exists(LAST_FILE):
+        return ""
+
+    with open(LAST_FILE) as f:
+        return f.read()
+
+
+def save_last(msg):
+
+    with open(LAST_FILE, "w") as f:
+        f.write(msg)
+
+
 today = datetime.date.today()
 
 candidates = []
 
-for i in range(1, 31):
+for i in range(1,31):
 
     target = today + datetime.timedelta(days=i)
 
     weekday = target.weekday()
 
-    if weekday in [3, 4, 6, 0]:
+    if weekday in [3,4,6,0]:
 
-        if weekday in [3, 4]:
+        if weekday in [3,4]:
             route = "東京 → 富山"
         else:
             route = "富山 → 東京"
 
-        candidates.append((target, route))
+        candidates.append((target,route))
 
 
-if candidates:
+message = "🚄富山おすすめランキング\n\n"
 
-    message = "🚄富山おすすめランキング\n\n"
+rank = 1
 
-    rank = 1
+for d,r in candidates[:5]:
 
-    for d, r in candidates[:5]:
-
-        message += f"""{rank}位
+    message += f"""{rank}位
 {d}
 {r}
 
 """
 
-        rank += 1
+    rank += 1
 
-    message += """
-👇えきねっと検索
+
+message += """
+👇えきねっと
 https://www.eki-net.com/
 """
 
+
+last = load_last()
+
+if message != last:
+
     send_line(message)
+    save_last(message)
